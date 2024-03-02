1b1076d6235c25a88bc4216f10731777b1a662f7
cesarzijp <czijp@me.com>
2024-03-02 13:10:51 +0100

    mobile fixes

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Container(props) {
   return (
-    <div className='w-full max-w-[900px] mx-auto flex px-4 '>
+    <div className='w-full max-w-full lg:max-w-[900px] mx-auto flex px-4 '>
       {props.children}
     </div>
   );
